e93866cad0d6ceaf86b9629a00616a88ba01f114
JyotiSathe <35442393+JyotiSathe@users.noreply.github.com>
2018-01-28 21:55:39 +0530

    Delete first_two_last_two.py

--- first_two_last_two.py
+++
@@ -1,9 +0,0 @@
-#WAP to accept a string from user and display a string made of first 2 characters and last 2 characters
-def two_letters_display(input_str):
-    return input_str[0:2]+input_str[-2:]
-def main():
-    input_str=input("Enter String")
-    output_str=two_letters_display(input_str)
-    print(output_str)
-if __name__=='__main__':
-    main()
