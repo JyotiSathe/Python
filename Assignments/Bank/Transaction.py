ebaced77e1b6a13cc1d2895fb570edf59eadafac
JyotiSathe <35442393+JyotiSathe@users.noreply.github.com>
2018-03-11 20:42:23 +0530

    Delete Transaction.py

--- Assignments/Bank/Transaction.py
+++
@@ -1,9 +0,0 @@
-
-
-class Transaction:
-    def __init__(self,timestamp,where,trans_type,amount,balance):
-        self.timestamp=timestamp
-        self.where=where
-        self.trans_type=trans_type
-        self.amount=amount
-        self.balance=balance
